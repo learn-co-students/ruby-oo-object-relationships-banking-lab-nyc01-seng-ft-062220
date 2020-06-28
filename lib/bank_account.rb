dfe4e3fe18b60957232036e06b1328d375d83906
jakemeout <jacobosity@gmail.com>
2020-06-28 19:03:16 -0400

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,37 @@
+require 'pry'
 class BankAccount
+    attr_reader :name
+    attr_accessor :balance, :status
+    @@all = []
+    
+    def initialize(name,balance=1000,status="open")
+            @name = name
+            @balance = balance
+            @status = status
+            # @broke = 0
+            @@all << self
+    end
+    
+    def deposit(money)
+        @balance += money
+    end
+
+    def self.all
+        @@all
+    end
+
+    def display_balance
+    #puts will return nil
+    "Your balance is $#{self.balance}."
+    end
+
+    def valid?
+        self.balance > 0 && status == "open"
+    end
+
+    def close_account
+        self.status = "closed"
+    end
 
 end
+
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,65 @@
+require 'pry'
 class Transfer
-  # your code here
+
+  attr_reader :sender, :receiver
+  attr_accessor :status, :amount
+
+  @@all = []
+  
+  def initialize(sender,receiver, status='pending', amount)
+    @sender = sender
+    @receiver = receiver
+    @status = status
+    @amount = amount
+    @@all << self 
+  end
+
+  def valid?
+    sender.valid?
+    receiver.valid?
+  end
+
+  def self.all
+    @@all
+  end
+
+  
+
+
+  def execute_transaction
+    if self.valid? && amount < @sender.balance && self.status != "complete"
+      
+      @sender.balance -= amount
+      @receiver.balance += amount
+      self.status = "complete"  
+    else
+      self.status = "rejected"
+        "Transaction rejected. Please check your account balance."
+      end 
+
+  end 
+
+  def reverse_transfer
+
+    last_transaction = Transfer.all.pop
+
+    if (self.valid? || amount > @sender.balance) && (last_transaction.status == "complete") 
+      # not that elegant... it would be best to save all the transactions in a hash like a transaction history. 
+      @sender.balance += last_transaction.amount
+      @receiver.balance -= last_transaction.amount
+       self.status = "reversed"
+    else
+      return "Whoops something went wrong"
+    end 
+  end
+ 
+   
+
 end
+
+# amanda = BankAccount.new("Amanda") 
+#  avi = BankAccount.new("Avi")
+#  transfer = Transfer.new(amanda, avi, 50)
+#  transfer.execute_transaction
+#    binding.pry
+#    0
